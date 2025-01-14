d0a17f901529275998e31d09c7aab5221896ae5d
Sagi <sagi16121960@gmail.com>
2025-01-14 10:57:53 +0200

    SQL SCRIPT DONE

--- MediaReviewerServer/DataBase/dataBaseCreation.sql
+++ MediaReviewerServer/DataBase/dataBaseCreation.sql
@@ -12,23 +12,20 @@ Go
 
 Create Table Users
 (
-Username nvarchar(16) Primary Key,
+UserID int Primary Key Identity,
+Username nvarchar(16) Not Null,
 Password nvarchar(16) Not Null,
 Firstname nvarchar(16) Not Null,
 Lastname nvarchar(16) Not Null,
 Email nvarchar(256) Not Null,
-IsAdmin bool Not Null
-)
-
-Create Table ContentTypes
-(
-TypeID int Primary Key Identity,
-TypeName nvarchar(16) Not Null
+IsAdmin bool Not Null,
+Image nvarchar
 )
 
 Create Table Genres
 (
-Genre nvarchar(16) Primary Key
+GenreID int Primary Key Identity, 
+GenreName nvarchar(16)
 )
 
 Create Table Contents
@@ -38,25 +35,45 @@ ContentName nvarchar(256) Not Null,
 ReleaseYear int Not Null,
 Length int Not Null,
 Description nvarchar(1024) Not Null,
-Rating Float,
-Image nvarchar(256) Not Null,
-Trailer nvarchar(256),
-Author nvarchar(256),
-Creator nvarchar(256),
-Director nvarchar(256),
-Star nvarchar(256),
-Writer nvarchar(256),
-MultiAuthors bool,
-MultiCreators bool,
-MultiDirectors bool,
-MultiStars bool,
-MultiWriters bool,
+Rating Float Not Null,
+Image nvarchar Not Null,
+)
+
+Create Table Movies
+(
+ContentID int Primary Key Foreign Key References Contents(ContentID),
+Trailer nvarchar Not Null,
+Director nvarchar(256) Not Null,
+Star nvarchar(256) Not Null,
+Writer nvarchar(256) Not Null,
+MultiDirectors bool Not Null,
+MultiStars bool Not Null,
+MultiWriters bool Not Null,
+)
+
+Create Table Series
+(
+ContentID int Primary Key Foreign Key References Contents(ContentID),
+Trailer nvarchar Not Null,
+Creator nvarchar(256) Not Null,
+Star nvarchar(256) Not Null,
+Writer nvarchar(256) Not Null,
+MultiCreators bool Not Null,
+MultiStars bool Not Null,
+MultiWriters bool Not Null,
+)
+
+Create Table Books
+(
+ContentID int Primary Key Foreign Key References Contents(ContentID),
+Author nvarchar(256) Not Null,
+MultiAuthors bool Not Null,
 )
 
 Create Table Reviews
 (
 ReviewID int Primary Key Identity,
-Username nvarchar Foreign Key References Users(Username),
+UserID int Foreign Key References Users(UserID),
 ContentID int Foreign Key References Contents(ContentID),
 Rating Float Not Null,
 Description nvarchar(1024) Not Null,
@@ -66,7 +83,7 @@ Date Nvarchar(16) Not Null
 Create Table Requests
 (
 RequestID int Primary Key Identity,
-Username nvarchar Foreign Key References Users(Username),
+UserID int Foreign Key References Users(UserID),
 Title nvarchar(256) Not Null,
 Description nvarchar(1024) Not Null,
 Date nvarchar(16) Not Null
@@ -74,6 +91,6 @@ Date nvarchar(16) Not Null
 
 Create Table GenresToContents
 (
-ContentID int Foreign Key References Contents(ContentID) Identity,
-Genre nvarchar Foreign Key References Genres(Genre)
+ContentID int Foreign Key References Contents(ContentID),
+GenreID int Foreign Key References Genres(GenreID)
 )
